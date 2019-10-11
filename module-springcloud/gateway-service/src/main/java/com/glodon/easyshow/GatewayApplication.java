6136f2146263377681832d5076bca86e10a7167e
Fwd@T460 <fanweidong2014@163.com>
2019-10-12 00:33:58 +0800

    spring gateway

---
+++ module-springcloud/gateway-service/src/main/java/com/glodon/easyshow/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.glodon.easyshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: fanwd
+ * @Description: 网关
+ * @Date: Create in 23:59 2019/10/11
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
