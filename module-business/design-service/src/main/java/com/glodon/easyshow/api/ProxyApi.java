49c180339bb68bf8d66fa5890036d37a98f51a4b
fanwd <fanwd@glodon.com>
2019-10-18 20:19:57 +0800

    添加代理请求类

---
+++ module-business/design-service/src/main/java/com/glodon/easyshow/api/ProxyApi.java
@@ -0,0 +1,14 @@
+package com.glodon.easyshow.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName ProxyApi
+ * @Description 代理API
+ * @Author fanwd
+ * @Date 2019/10/18 20:18
+ **/
+@RestController
+public class ProxyApi {
+
+}
