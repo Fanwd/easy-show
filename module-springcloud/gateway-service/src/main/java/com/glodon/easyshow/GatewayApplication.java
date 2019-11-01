ff2dacfd8ff1c67564225bbbcada2c1898b3e672
fanwd <fanwd@glodon.com>
2019-11-01 19:06:17 +0800

    perf(design): 测试代码

--- module-springcloud/gateway-service/src/main/java/com/glodon/easyshow/GatewayApplication.java
+++ module-springcloud/gateway-service/src/main/java/com/glodon/easyshow/GatewayApplication.java
@@ -1,7 +1,12 @@
 package com.glodon.easyshow;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @Author: fanwd
@@ -13,4 +18,16 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
+//    @Autowired
+//    private TokenRelayGatewayFilterFactory filterFactory;
+//
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("resource", r -> r.path("/resource")
+//                        .filters(f -> f.filter(filterFactory.apply()))
+//                        .uri("http://localhost:9000"))
+//                .build();
+//    }
 }
