975752b033b8404ede99440a993501dd939551e3
Fwd@T460 <fanweidong2014@163.com>
2019-11-03 23:54:35 +0800

    feat(sso): 添加sso服务

---
+++ module-base/sso-service/src/main/java/com/glodon/easyshow/SsoApplication.java
@@ -0,0 +1,17 @@
+package com.glodon.easyshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName SsoApplication
+ * @Description: sso服务
+ * @Author: fanwd
+ * @Date: Create in 23:31 2019/11/3
+ */
+@SpringBootApplication
+public class SsoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SsoApplication.class, args);
+    }
+}
